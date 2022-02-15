970cc9b685c0053a22b71d49cdde4e6ff9084a14
Flukavka <95244493+Flukavka@users.noreply.github.com>
2022-02-16 00:05:38 +0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-let mainscreen_dark = document.querySelector('.mainscreen_dark');
-let menu_image = document.querySelector('.menu_image');
-
-menu_image.addEventListener('click', function() {
-    mainscreen_dark.classList.toggle('hidden');
-});
